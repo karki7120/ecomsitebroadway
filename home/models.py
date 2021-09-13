ea57df04bac629b7ce9773445a21c784d6980ac5
karki7120 <kkarki761@gmail.com>
2021-09-13 18:00:46 +0545

    models created

--- home/models.py
+++ home/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
 # Create your models here.
+
+STATUS = (('active', 'active'),('', 'inactive'))
+
+class Category(models.Model):
+    name = models.CharField(max_length= 300)
+    slug = models.CharField(max_length= 300, unique=True)
+    description = models.CharField(max_length= 300)
+
+
+    def __str__(self):
+        return self.name
+
+
+
+
+class SubCategory(models.Model):
+    name = models.CharField(max_length= 300)
+    slug = models.CharField(max_length= 300)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    description = models.TextField(max_length= 300)
+
+
+    def __str__(self):
+        return self.name
+
+class slider(models.Model):
+    name = models.CharField(max_length= 300)
+    slug = models.CharField(max_length= 300)
+    description = models.TextField(max_length= 300)
+    image = models.ImageField(upload_to = 'media' )
+    rank = models.IntegerField()
+    status = models.CharField(max_length= 300, choices = STATUS)
+
+    def __str__(self):
+        return self.name
